30314d7e1e1461aff9cd324367515ae2afd9d4e9
小野 慶太 <dorokei@gmail.com>
2020-06-02 10:03:24 +0900

    Change data structure

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,26 +4,13 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    private final int maxLength;
-    private final Cell[][] grids;
-    private int openNum;
-    private final int topRootIndex;
-    private final int bottomRootIndex;
-
-    private class Cell {
-        private int parentRow;
-        private int parentCol;
-        private boolean isOpen;
-        private int sz;
-
-        public Cell(int x, int y) {
-            parentRow = x;
-            parentCol = y;
-            isOpen = false;
-            sz = 1;
-        }
-    }
+    private final WeightedQuickUnionUF uf;
+    private boolean[] openArr;
+    private final int length;
+    private int openSum;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -31,181 +18,120 @@ public class Percolation {
             throw new IllegalArgumentException();
         }
 
-        topRootIndex = n;
-        bottomRootIndex = n + 1;
-        grids = new Cell[n + 2][n + 2];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0) {
-                    grids[i][j] = new Cell(topRootIndex, topRootIndex);
-                }
-                else if (i == n - 1) {
-                    grids[i][j] = new Cell(bottomRootIndex, bottomRootIndex);
-                }
-                else {
-                    grids[i][j] = new Cell(i, j);
-                }
-            }
+        length = n;
+        openSum = 0;
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        openArr = new boolean[n * n + 2];
+
+        // Connect top
+        for (int i = 1; i <= n; i++) {
+            uf.union(0, i);
         }
-        // Top root cell
-        Cell top = new Cell(topRootIndex, topRootIndex);
-        top.sz = n;
-        grids[topRootIndex][topRootIndex] = top;
 
-        // Bottom root cell
-        Cell bottom = new Cell(bottomRootIndex, bottomRootIndex);
-        bottom.sz = n;
-        grids[bottomRootIndex][bottomRootIndex] = bottom;
-
-        openNum = 0;
-        maxLength = n;
+        // Connect bottom
+        for (int i = getPositonFromIndex(n, 1); i <= getPositonFromIndex(n, n); i++) {
+            uf.union(i, n * n + 1);
+        }
     }
 
-    private Cell root(Cell c) {
-        int a = c.parentRow;
-        int b = c.parentCol;
-        while (!(a == grids[a][b].parentRow &&
-                b == grids[a][b].parentCol)) {
-            Cell cell = grids[a][b];
-            a = cell.parentRow;
-            b = cell.parentCol;
-        }
-        return grids[a][b];
+    private void checkCanonical(int row, int col) {
+        checkRowCol(row, col);
+        int index = getPositonFromIndex(row, col);
+        System.out.println(row + "," + col + " : " + uf.find(index));
     }
 
-    private void checkRoot(int row, int col) {
-        row -= 1;
-        col -= 1;
-        Cell c = grids[row][col];
-        Cell root = root(c);
-        System.out.println(row + ", " + col + "'s parents: " + c.parentRow + ", " + c.parentCol);
-        System.out.println(row + ", " + col + "'s root: " + root.parentRow + ", " + root.parentCol);
+    private void checkRowCol(int row, int col) {
+        if (row <= 0 || col <= 0 || row > length || col > length) {
+            throw new IllegalArgumentException(row + "," + col + " length:" + length);
+        }
+    }
+
+    private int getPositonFromIndex(int row, int col) {
+        checkRowCol(row, col);
+        return (row - 1) * length + col;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        row -= 1;
-        col -= 1;
-        if (row >= maxLength || col >= maxLength) {
-            throw new IllegalArgumentException(
-                    "row:" + row + ", col:" + col + ", maxLength:" + maxLength);
+        checkRowCol(row, col);
+        int index = getPositonFromIndex(row, col);
+
+        // Open
+        if (openArr[index]) return;
+
+        openArr[index] = true;
+        openSum++;
+
+        // Connect
+        // Top
+        if (1 < row) {
+            int i = getPositonFromIndex(row - 1, col);
+            if (openArr[i]) uf.union(index, i);
         }
 
-        Cell c = grids[row][col];
-        if (c.isOpen) return;
-
-        c.isOpen = true;
-        openNum++;
-
-        // top
-        if (row - 1 >= 0 && grids[row - 1][col].isOpen) {
-            connect(c, grids[row - 1][col]);
+        // Right
+        if (col < length) {
+            int i = getPositonFromIndex(row, col + 1);
+            if (openArr[i]) uf.union(index, i);
         }
 
-        // right
-        if (col + 1 <= maxLength - 1 && grids[row][col + 1].isOpen) {
-            connect(c, grids[row][col + 1]);
+        // Bottom
+        if (row < length) {
+            int i = getPositonFromIndex(row + 1, col);
+            if (openArr[i]) uf.union(index, i);
         }
 
-        // bottom
-        if (row + 1 <= maxLength - 1 && grids[row + 1][col].isOpen) {
-            connect(c, grids[row + 1][col]);
+        // Left
+        if (1 < col) {
+            int i = getPositonFromIndex(row, col - 1);
+            if (openArr[i]) uf.union(index, i);
         }
-
-        // left
-        if (col - 1 >= 0 && grids[row][col - 1].isOpen) {
-            connect(c, grids[row][col - 1]);
-        }
-    }
-
-    private void connect(Cell a, Cell b) {
-        Cell rootA = root(a);
-        Cell rootB = root(b);
-        if (rootA.parentRow == rootB.parentRow && rootA.parentCol == rootB.parentCol) return;
-
-        if (rootA.sz > rootB.sz) {
-            // Add B to A
-            rootB.parentRow = rootA.parentRow;
-            rootB.parentCol = rootA.parentCol;
-            rootA.sz += rootB.sz;
-        }
-        else {
-            // Add A to B
-            rootA.parentRow = rootB.parentRow;
-            rootA.parentCol = rootB.parentCol;
-            rootB.sz += rootA.sz;
-        }
-    }
-
-    private boolean connected(Cell a, Cell b) {
-        Cell rootA = root(a);
-        Cell rootB = root(b);
-        return rootA.parentRow == rootB.parentRow && rootA.parentCol == rootB.parentCol;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        row -= 1;
-        col -= 1;
-        if (row >= maxLength || col >= maxLength) {
-            throw new IllegalArgumentException(
-                    "row:" + row + ", col:" + col + ", maxLength:" + maxLength);
-        }
-        return grids[row][col].isOpen;
+        checkRowCol(row, col);
+        int index = getPositonFromIndex(row, col);
+        return openArr[index];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        row -= 1;
-        col -= 1;
-        if (row >= maxLength || col >= maxLength) {
-            throw new IllegalArgumentException(
-                    "row:" + row + ", col:" + col + ", maxLength:" + maxLength);
-        }
-
-        Cell c = grids[row][col];
-        Cell topRoot = grids[topRootIndex][topRootIndex];
-        return connected(c, topRoot);
+        checkRowCol(row, col);
+        int index = getPositonFromIndex(row, col);
+        return uf.find(0) == uf.find(index) && openArr[index];
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return openNum;
+        return openSum;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        Cell topRoot = grids[topRootIndex][topRootIndex];
-        Cell bottomRoot = grids[bottomRootIndex][bottomRootIndex];
-        return connected(bottomRoot, topRoot);
+        return uf.find(0) == uf.find(length * length + 1);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        test1();
-    }
-
-    private static void test1() {
         Percolation p = new Percolation(3);
 
-        p.checkRoot(1, 2);
+        p.checkCanonical(1, 2);
         System.out.println("Open 1,2");
         p.open(1, 2);
-        p.checkRoot(1, 2);
+        p.checkCanonical(1, 2);
 
-        p.checkRoot(2, 2);
+        p.checkCanonical(2, 2);
         System.out.println("Open 2,2");
         p.open(2, 2);
-        p.checkRoot(2, 2);
+        p.checkCanonical(2, 2);
 
-        p.checkRoot(3, 2);
         System.out.println("Open 3,2");
         p.open(3, 2);
-        p.checkRoot(3, 2);
+        p.checkCanonical(3, 2);
 
         if (p.isFull(2, 2)) System.out.println("2,2 is full.");
         if (p.percolates()) System.out.println("percolates!");
-        // p.checkRoot(4, 4);
-        // p.checkRoot(3, 3);
+
     }
 }
